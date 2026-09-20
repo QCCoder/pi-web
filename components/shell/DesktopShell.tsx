076dec1317730f7ddcda039eb365eab82bc31d75
钱城 <qiancheng@qnvip.com>
2026-09-20 15:03:50 +0800

    no message

--- components/shell/DesktopShell.tsx
+++ components/shell/DesktopShell.tsx
@@ -108,7 +108,6 @@ export function DesktopShell() {
     closeTab,
     closeTabs,
     openSessionTab,
-    openNewSessionTab,
     handleSelectTab,
     handleSessionRemoved,
     handleCreateWorkspace,
